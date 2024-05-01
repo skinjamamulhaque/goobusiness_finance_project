24acc604b2212c8d909de1ef2d75e86d33de55c0
Sk Injamamul Haque <injamamulhaque76@gmail.com>
2024-05-01 17:22:10 +0530

    build full website

---
+++ js/homePage/script.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+// const carouselChildrens = [...carousel.children];
+
+
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+
